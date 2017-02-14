fada5603ac4c764214814786635889e6db8b5966
Zhou Wang <zhouwang@microsoft.com>
2017-02-14 17:53:24 +0100

    add c++ examples for GPU

--- Examples/Evaluation/CNTKLibraryCPPEvalGPUExamples/CNTKLibraryCPPEvalGPUExamples.cpp
+++ Examples/Evaluation/CNTKLibraryCPPEvalGPUExamples/CNTKLibraryCPPEvalGPUExamples.cpp
@@ -2,7 +2,7 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE.md file in the project root for full license information.
 //
-// CNTKLibraryCPPEvalCPUOnlyExamples.cpp : Sample application shows how to evaluate a model using CNTK V2 API.
+// CNTKLibraryCPPEvalGPUExamples.cpp : Sample application shows how to evaluate a model using CNTK V2 API.
 //
 
 #include <stdio.h>
@@ -12,8 +12,8 @@ void MultiThreadsEvaluation(bool);
 int main()
 {
 
-    fprintf(stderr, "\n##### Run CNTKLibraryCPPEvalCPUOnlyExamples on CPU. #####\n");
-    MultiThreadsEvaluation(false);
+    fprintf(stderr, "\n##### Run CNTKLibraryCPPEvalGPUExamples on CPU and GPU. #####\n");
+    MultiThreadsEvaluation(true);
 
     fprintf(stderr, "Evaluation complete.\n");
     fflush(stderr);
